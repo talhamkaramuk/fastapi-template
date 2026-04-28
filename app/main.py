3c9bcbfa1905ac93a0c1de1cedf3b12c24fb7647
Talha Mehmet KARAMUK <84032686+talhamkaramuk@users.noreply.github.com>
2026-04-29 00:47:35 +0300

    Update FastAPI app title and router inclusion

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
+from app.api.router import api_router
 from fastapi import FastAPI
 
-from app.api.v1.routers.health import router as health_router
-
 app = FastAPI(
-    title="FastAPI Template",
+    title="YOUR APP NAME",
     version="1.0.0",
 )
 
-# Router'ları ekle
-app.include_router(health_router, prefix="/api/v1")
+app.include_router(api_router)
